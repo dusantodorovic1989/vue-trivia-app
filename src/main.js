0f947fe7ef3acff3b5df0c1e10857d2d54307615
Dusan Todorovic <dusantodorovic1989@gmail.com>
2018-12-07 19:24:47 +0100

    zadatak 2

--- src/main.js
+++ src/main.js
@@ -2,10 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 import TriviaApp from './components/TriviaApp.vue'
+import Vuex from 'vuex'
+import { chuckStore } from './store/chuckStore.js'
+import {triviaStore} from './store/triviaStore.js'
 
 Vue.use(VueRouter)
 
+Vue.use(Vuex)
+
 Vue.config.productionTip = false
+
+const store = new Vuex.Store({
+  modules:{
+    chuckStore,
+    triviaStore
+  }
+})
 
 const routes = [
   {path:'/', redirect:'/trivia'},
@@ -19,6 +31,7 @@ const router = new VueRouter({
 
 
 new Vue({
+  store,
   router,
   render: h => h(App),
 }).$mount('#app')
